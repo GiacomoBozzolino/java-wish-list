5684597579080c1b0df2f74e751fe423639a561d
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-11-17 12:05:19 +0100

    first commit

---
+++ src/org/lessons/java/christmas/WishList.java
@@ -0,0 +1,14 @@
+package org.lessons.java.christmas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class WishList {
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		
+		List<String> values = new ArrayList();
+	
+	}
+}
---
+++ src/org/lessons/java/christmas/pojo/Gift.java
@@ -0,0 +1,9 @@
+package org.lessons.java.christmas.pojo;
+
+public class Gift {
+	
+	public static String gift;
+	public static String receiver;
+	
+
+}
