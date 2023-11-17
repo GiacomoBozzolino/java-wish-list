9650a11dbaf7cb8ea82845142890e2d202c3fb81
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-11-17 12:29:55 +0100

    esercizio base completato

--- src/org/lessons/java/christmas/WishList.java
+++ src/org/lessons/java/christmas/WishList.java
@@ -1,6 +1,7 @@
 package org.lessons.java.christmas;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,7 +9,35 @@ public class WishList {
 	public static void main(String[] args) {
 		Scanner in = new Scanner (System.in);
 		
-		List<String> values = new ArrayList();
+		List<String> gifts = new ArrayList<>();
+		
+		while (true) {
+            System.out.print("Inserisci il nome del regalo: ");
+            String regalo = in.nextLine();
+            
+            
+         // Aggiungere il regalo alla lista
+            gifts.add(regalo);
+            
+            System.out.println("Lunghezza della lista: " + gifts.size());
+            
+            System.out.println("Vuoi aggiungere un altro regalo?");
+            String gift=in.nextLine();
+
+            // Verificare se l'utente vuole fermarsi
+            if ( gift.equals("no")) {
+                break;
+            }  
+		}
+		in.close();
+		
+		Collections.sort(gifts);
+		
+		 System.out.println("Lista dei regali di Natale:");
+
+	        for (String regalo : gifts) {
+	            System.out.println("- " + regalo);
+	        }
 	
 	}
 }
