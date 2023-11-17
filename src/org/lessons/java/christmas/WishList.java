fc8e9ced1b2c26012634e41d96e6bd248de609af
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-11-17 14:01:03 +0100

    bonus completato

--- src/org/lessons/java/christmas/WishList.java
+++ src/org/lessons/java/christmas/WishList.java
@@ -2,42 +2,85 @@ package org.lessons.java.christmas;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+
+import org.lessons.java.christmas.pojo.Gift;
 
 public class WishList {
 	public static void main(String[] args) {
 		Scanner in = new Scanner (System.in);
 		
-		List<String> gifts = new ArrayList<>();
+//		List<String> gifts = new ArrayList<>();
+//		
+//		while (true) {
+//            System.out.print("Inserisci il nome del regalo: ");
+//            String regalo = in.nextLine();
+//            
+//            
+//         // Aggiungere il regalo alla lista
+//            gifts.add(regalo);
+//            
+//            System.out.println("Lunghezza della lista: " + gifts.size());
+//            
+//            System.out.println("Vuoi aggiungere un altro regalo?");
+//            String gift=in.nextLine();
+//
+//            // Verificare se l'utente vuole fermarsi
+//            if ( gift.equals("no")) {
+//                break;
+//            }  
+//		}
+//		in.close();
+//		
+//		Collections.sort(gifts);
+//		
+//		 System.out.println("Lista dei regali di Natale:");
+//
+//	        for (String regalo : gifts) {
+//	            System.out.println("- " + regalo);
+//	        }
 		
-		while (true) {
-            System.out.print("Inserisci il nome del regalo: ");
-            String regalo = in.nextLine();
-            
-            
-         // Aggiungere il regalo alla lista
-            gifts.add(regalo);
-            
-            System.out.println("Lunghezza della lista: " + gifts.size());
-            
-            System.out.println("Vuoi aggiungere un altro regalo?");
-            String gift=in.nextLine();
+		 List<Gift> gifts = new ArrayList<>();
+	        
 
-            // Verificare se l'utente vuole fermarsi
-            if ( gift.equals("no")) {
-                break;
-            }  
-		}
-		in.close();
-		
-		Collections.sort(gifts);
-		
-		 System.out.println("Lista dei regali di Natale:");
+	        while (true) {
+	            System.out.print("Inserisci il nome del regalo: ");
+	            String nomeRegalo = in.nextLine();
 
-	        for (String regalo : gifts) {
-	            System.out.println("- " + regalo);
+	            System.out.print("A chi è destinato il regalo? (mamma, papà, cugino, ...): ");
+	            String destinatario = in.nextLine();
+
+	            Gift regalo = new Gift(nomeRegalo, destinatario);
+	            gifts.add(regalo);
+
+	            System.out.println("Lunghezza della lista: " + gifts.size());
+
+	            System.out.println("Vuoi aggiungere un altro regalo? (sì/no)");
+	            String risposta = in.nextLine().toLowerCase();
+
+	            if (!risposta.equals("sì") || !risposta.equals("si")) {
+	                break;
+	            }
 	        }
-	
-	}
+
+	        System.out.print("Vuoi ordinare la lista per nome o per destinatario? ");
+	        String ordinamento = in.nextLine().toLowerCase();
+
+	        if (ordinamento.equals("nome")) {
+	            Collections.sort(gifts, Comparator.comparing(regalo -> regalo.getName()));
+	        } else if (ordinamento.equals("destinatario")) {
+	            Collections.sort(gifts, Comparator.comparing(regalo -> regalo.getReceiver()));;
+	        } 
+
+	        in.close();
+
+	        System.out.println("Lista dei regali di Natale:");
+
+	        for (Gift regalo : gifts) {
+	        	System.out.println("Nome: " + regalo.getName() + ", Destinatario: " + regalo.getReceiver());
+	        }
+    }
+
 }
--- src/org/lessons/java/christmas/pojo/Gift.java
+++ src/org/lessons/java/christmas/pojo/Gift.java
@@ -2,8 +2,37 @@ package org.lessons.java.christmas.pojo;
 
 public class Gift {
 	
-	public static String gift;
-	public static String receiver;
+	 String name;
+     String receiver;
+	
+	public Gift(String name, String receiver) {
+		// TODO Auto-generated constructor stub
+		setName(name);
+		setReceiver(receiver);
+	}
+
+	public  String getName() {
+		return name;
+	}
+
+	public  void setName(String name) {
+		this.name = name;
+	}
+
+	public  String getReceiver() {
+		return receiver;
+	}
+
+	public  void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return getName()+ "\n"
+				+ getReceiver();
+	}
 	
 
 }
